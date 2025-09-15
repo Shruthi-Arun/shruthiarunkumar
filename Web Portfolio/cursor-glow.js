e46cfee87abaaa50ac4c8cb4e20261bfda935c30
Shruthi-Arun <121600283+Shruthi-Arun@users.noreply.github.com>
2025-09-15 16:25:26 -0400

    Delete Web Portfolio directory

--- Web Portfolio/cursor-glow.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("mousemove", (event) => {
-    const cursor = document.querySelector('.cursor-glow');
-    cursor.style.left = `${event.clientX}px`;
-    cursor.style.top = `${event.clientY}px`;
-});
